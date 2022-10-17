aa5c696e00f802f39b3833bb21758e1959e85181
NguyenQMinh01 <minhmon112233@gmail.com>
2022-10-17 22:55:21 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# == Route Map
+#
+
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  use_doorkeeper
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  namespace :api do
+    namespace :v1 do
+      resources :profile,   only: [:index]
+      resources :products,  only: [:index]
+    end
+  end
+
+  resources :product,   only: %i[index show]
+  resources :brands,    only: %i[index show]
+  resources :category,  only: [:show]
+  resources :search,    only: [:index]
+
+  resource :cart, only: %i[destroy show] do
+    resources :items, only: %i[destroy create]
+  end
+
+  root to: 'main#index'
+end
